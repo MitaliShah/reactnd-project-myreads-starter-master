a0535be458e357af3756719d23db8819513ca425
Mitali Shah <mitalishaleenshah@gmail.com>
2018-10-14 16:16:20 -0700

    fixed error

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -20,11 +20,7 @@ this.setState({
 updateSearchedBooks =(query)=>{
   if(query){
     BooksAPI.search(query).then((searchedBooks)=> {
-      if(searchedBooks.error){
-        this.setState({ searchedBooks: [] });
-      }else {
-        this.setState({searchedBooks: searchedBooks})
-      }    
+      this.setState({searchedBooks})
         })
     .catch(() => this.setState({ searchedBooks: [] })); 
     }else{
@@ -55,19 +51,19 @@ updateSearchedBooks =(query)=>{
             <div className="search-books-results">
               <ol className="books-grid">
 				      {
-                  this.state.map(searchedBook => {
+                  this.state.searchedBooks.map(searchedBooks => {
                     let shelf ="none";
 
                     this.props.books.map(book =>(
-                      book.id === searchedBook.id ?
+                      book.id === searchedBooks.id ?
                       shelf = book.shelf : ''
                     ));
 
                       
                     return(
-                      <li key={searchedBook.id}>
+                      <li key={searchedBooks.id}>
 					        <Book 
-                  book={searchedBook}
+                  book={searchedBooks}
                   moveShelf={this.props.moveShelf}
                   currentShelf={shelf}
 					        />
